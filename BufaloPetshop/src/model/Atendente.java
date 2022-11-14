8c63e0f7fd604553efadf638804e095f62d33b04
Marcos Lira <53446701+Marcoslira91@users.noreply.github.com>
2022-11-14 12:01:52 -0300

    Atualização

--- BufaloPetshop/src/model/Atendente.java
+++ BufaloPetshop/src/model/Atendente.java
@@ -1,11 +1,11 @@
 package model;
 
-public class Atendente extends Pessoa{
-	
+public class Atendente extends Pessoa {
+
 	// Construtor de Atendente
-	public Atendente(int id_Pessoa, String nome, long cpf, char sexo, long telefone, String endereco) {
-		super(id_Pessoa, nome, cpf, sexo, telefone, endereco);
+	public Atendente(int idPessoa, String nome, long cpf, char sexo, long telefone, String email, String endereco) {
+		super(idPessoa, nome, cpf, sexo, telefone, email, endereco);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 }
