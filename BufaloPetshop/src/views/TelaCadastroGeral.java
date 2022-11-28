c52e78299270232fac2fffc6e6181caccb44ff87
Davi Lisias <110578510+davilisias@users.noreply.github.com>
2022-11-28 19:42:20 -0300

    Update final
    Update final

--- BufaloPetshop/src/views/TelaCadastroGeral.java
+++ BufaloPetshop/src/views/TelaCadastroGeral.java
@@ -35,7 +35,6 @@ public class TelaCadastroGeral extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaCadastroGeral() {
-	//	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
