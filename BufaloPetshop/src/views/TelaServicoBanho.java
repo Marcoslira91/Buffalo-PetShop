c92a941a80d193a203aa5c15201a84c3b7c1aeb1
Davi Lisias <110578510+davilisias@users.noreply.github.com>
2022-11-28 19:56:15 -0300

    Update TelaServicoBanho.java

--- BufaloPetshop/src/views/TelaServicoBanho.java
+++ BufaloPetshop/src/views/TelaServicoBanho.java
@@ -4,8 +4,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.util.Random;
 
-import factory.db; 
+import factory.ConnectionFactory;
+import model.Pet;
 
 import java.awt.EventQueue;
 import java.awt.Font;
@@ -19,24 +21,30 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+
+import dao.PetDAO;
+
 import javax.swing.JComboBox;
 import javax.swing.JToggleButton;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JTextArea;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class TelaServicoBanho extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 	private JTextField jnome;
 	private JTextField jtipo;
 	private JTextField jraca;
-	private JTextField jpeso;
-	private JTextField jpesquisa;
-	private JTable table;
+	private JTextField jid;
 
 	/**
 	 * Launch the application.
@@ -59,7 +67,7 @@ public class TelaServicoBanho extends JFrame {
 	 */
 	public TelaServicoBanho() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 574, 397);
+		setBounds(100, 100, 610, 402);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -68,7 +76,7 @@ public class TelaServicoBanho extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("BuffalosPetShop");
 		lblNewLabel.setFont(new Font("Agency FB", Font.ITALIC, 14));
-		lblNewLabel.setBounds(479, 11, 69, 17);
+		lblNewLabel.setBounds(515, 11, 69, 17);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel(" Serviço selecionado: ");
@@ -92,55 +100,32 @@ public class TelaServicoBanho extends JFrame {
 		lblNewLabel_3.setBounds(169, 19, 46, 14);
 		contentPane.add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_2 = new JLabel("Digite o ID do seu pet:");
-		lblNewLabel_2.setBounds(10, 63, 136, 17);
-		contentPane.add(lblNewLabel_2);
-		
-		JButton btnNewButton = new JButton("Buscar");
+		JButton btnNewButton = new JButton("Buscar Pets");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {
-					Class.forName("com.mysql.cj.jbdc.Driver");
-					Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/BuffalosPet");
-					java.sql.Statement s= db.createConnectionToMySQL().createStatement();
-					String query= "SELECT * from Pet where nome= ?";
-					//s.executeQuery(query);
-					PreparedStatement pst = (PreparedStatement) db.createConnectionToMySQL();
-					pst.setString(1, jpesquisa.getText());
-					ResultSet rs = pst.executeQuery(query);
-					//ResultSetMetaData rsmd= rs.getMetaData();
-			/*
-					DefaultTableModel model=(DefaultTableModel)table.getModel();
-				
-					int cols= rsmd.getColumnCount();
-					String[]colName= new String[cols];
-					for (int i=0; i<cols;i++) {
-						colName[i]=rsmd.getColumnName(i+1);
-						model.setColumnIdentifiers(colName);
-					}
-					*/
-				
-				if(rs.next()) {
-						jnome.setText(rs.getString("nome"));
-						jtipo.setText(rs.getString("tipo"));
-						jraca.setText(rs.getString("raca"));
-						jpeso.setText(rs.getString("peso"));
-						
-						
-						
-					}else {
-						System.out.println("erro");
-					}
+				DefaultTableModel modelo= (DefaultTableModel) table.getModel();
+				 PetDAO dao = new PetDAO();
+				 try {
+					 for(Pet p: dao.listarTodosPets()) {
+						 modelo.addRow(new Object[] {
+				p.getIdPet(),
+				p.getNome(),
+				p.getTipo(),
+				p.getRaca(),
+				p.getSexo(),
+				p.getPedigree(),
+				p.getDataNascimento()			 
+						 });
+					 }
 					 
-				}
-				
+				 }
 				catch(Exception e1) {
-				
+					JOptionPane.showMessageDialog(null,"ERRO!");
 				}
-	
+		 
 			}
 		});
-		btnNewButton.setBounds(252, 60, 89, 23);
+		btnNewButton.setBounds(10, 173, 122, 23);
 		contentPane.add(btnNewButton);
 		
 		JLabel lblNewLabel_5 = new JLabel("Descrição:");
@@ -151,44 +136,108 @@ public class TelaServicoBanho extends JFrame {
 		textArea.setBounds(149, 289, 270, 45);
 		contentPane.add(textArea);
 		
-		JButton btnNewButton_1 = new JButton("Concluir");
+		JButton btnNewButton_1 = new JButton("Selecionar pet\r\n");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				dispose();
-				TelaServicoDecisao tela= new TelaServicoDecisao();
-				tela.setVisible(true);
+			 
+				switch(JOptionPane.showConfirmDialog(null, "ID:"+jid.getText()+"\n"+"Nome:"+jnome.getText()
+				+"\n"+"Tipo:"+jtipo.getText()
+				,"Confrimar serviço para:", JOptionPane.YES_NO_OPTION))
+				{
+				case 0:
+					Random random= new Random();
+					int senha=random.nextInt(1000);
+					JOptionPane.showMessageDialog(null,"ID:"+jid.getText()+"\n"+"Nome:"+jnome.getText()
+					+"\n"+"Tipo:"+jtipo.getText() +"\nTotal:\nR$:50,00", "VALOR", JOptionPane.INFORMATION_MESSAGE);
+					JOptionPane.showMessageDialog(null,"Sua senha é: "+ senha);
+					dispose();
+					TelaServicoDecisao tela= new TelaServicoDecisao();
+					 tela.setVisible(true);
+					break;
+				case 1:
+					JOptionPane.showMessageDialog(null, "Selecione um pet para continuar", "AVISO", JOptionPane.INFORMATION_MESSAGE);
+					dispose();
+					TelaServicoBanho tela1= new TelaServicoBanho();
+					tela1.setVisible(true);
+					break;
+				}
+				 
 			}
 		});
-		btnNewButton_1.setBounds(459, 324, 89, 23);
+		btnNewButton_1.setBounds(459, 324, 125, 23);
 		contentPane.add(btnNewButton_1);
 		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Pet p= new Pet();
+				PetDAO dao= new PetDAO();
+				int index= table.getSelectedRow();
+				
+				p= dao.listarTodosPets().get(index);
+				
+				jnome.setText(p.getNome());
+				jtipo.setText(p.getTipo());
+				jraca.setText(p.getRaca());
+				jid.setText(String.valueOf(p.getIdPet()));
+	 
+			}
+		});
+		scrollPane.setBounds(10, 39, 551, 123);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Id", "Nome", "Tipo", "Raca", "Sexo", "Pedigree", "Datanasc"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(23);
+		table.getColumnModel().getColumn(4).setPreferredWidth(35);
+		table.getColumnModel().getColumn(6).setPreferredWidth(63);
+		
 		jnome = new JTextField();
-		jnome.setBounds(10, 91, 89, 31);
+		jnome.setBounds(498, 207, 86, 20);
 		contentPane.add(jnome);
 		jnome.setColumns(10);
 		
 		jtipo = new JTextField();
-		jtipo.setBounds(10, 133, 89, 21);
+		jtipo.setBounds(498, 238, 86, 20);
 		contentPane.add(jtipo);
 		jtipo.setColumns(10);
 		
+		JLabel lblNewLabel_2 = new JLabel("Nome:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_2.setBounds(453, 210, 42, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_4 = new JLabel("Tipo:\r\n");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_4.setBounds(459, 241, 36, 14);
+		contentPane.add(lblNewLabel_4);
+		
 		jraca = new JTextField();
-		jraca.setBounds(10, 165, 89, 29);
-		contentPane.add(jraca);
 		jraca.setColumns(10);
+		jraca.setBounds(498, 269, 86, 20);
+		contentPane.add(jraca);
 		
-		jpeso = new JTextField();
-		jpeso.setBounds(10, 205, 89, 25);
-		contentPane.add(jpeso);
-		jpeso.setColumns(10);
+		JLabel lblNewLabel_6_1 = new JLabel("Raca:");
+		lblNewLabel_6_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_6_1.setBounds(455, 275, 42, 14);
+		contentPane.add(lblNewLabel_6_1);
 		
-		jpesquisa = new JTextField();
-		jpesquisa.setBounds(139, 61, 76, 20);
-		contentPane.add(jpesquisa);
-		jpesquisa.setColumns(10);
+		jid = new JTextField();
+		jid.setColumns(10);
+		jid.setBounds(498, 174, 46, 20);
+		contentPane.add(jid);
 		
-		table = new JTable();
-		table.setBounds(262, 94, 260, 164);
-		contentPane.add(table);
+		JLabel lblNewLabel_7 = new JLabel("Id:");
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_7.setBounds(472, 177, 23, 14);
+		contentPane.add(lblNewLabel_7);
 	}
 }
